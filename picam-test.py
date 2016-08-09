b05f239eaadc7c7fa0fe955771ebd528eee78d58
levisonofed <levi.j.elston@gmail.com>
2016-08-08 21:36:43 -0400

    Update picam-test.py

--- picam-test.py
+++ picam-test.py
@@ -8,16 +8,17 @@ camera = PiCamera()
 
 camera.hflip=True
 camera.vflip=True
-camera.resolution=(1024,768)
+#camera.resolution=(1024,768)
+camera.resolution=(320,240)
 
-for i in range(10):
+for i in range(4):
 	camera.capture('image{0:04d}.jpg'.format(i))
 	sleep(.5)
 	
 endTime = time.time()
 print(endTime - startTime, ' seconds')
 	
-system('convert -delay 10 -loop 0 image*.jpg animation.gif')
+system('convert -delay 25 -loop 0 image*.jpg animation.gif')
 	
 #camera.start_recording('video.h264')
 #sleep (5)
