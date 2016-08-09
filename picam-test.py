9e9d40fa66f9954f8423ef7088dee0b6ad93d5b8
levisonofed <levi.j.elston@gmail.com>
2016-08-08 22:08:40 -0400

    Update picam-test.py

--- picam-test.py
+++ picam-test.py
@@ -9,7 +9,7 @@ camera = PiCamera()
 camera.hflip=True
 camera.vflip=True
 #camera.resolution=(1024,768)
-camera.resolution=(320,240)
+camera.resolution=(640,480)
 
 #camera warm up
 camera.start_preview()
@@ -17,7 +17,7 @@ sleep(2)
 
 #camera capture
 print('capture start')
-for i in range(5):
+for i in range(10):
 	camera.capture('image{0:04d}.jpg'.format(i))
 	sleep(.5)
 camera.close()
@@ -27,7 +27,9 @@ print('capture end, time elapsed=',endTime - startTime, 's')
 
 #convert jpgs to gif
 print('convert to gif start')
-system('convert -delay 50 -loop 0 image*.jpg animation.gif')
+system('convert -delay 100 -loop 0 image*.jpg animation.gif')
 
 endTime = time.time()
 print('done, total time=', endTime-startTime, 's')
+
+#email image
