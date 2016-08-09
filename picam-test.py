9604580b9658a0d6cdd1abf97a75baac7eaf701e
levisonofed <levi.j.elston@gmail.com>
2016-08-08 21:00:44 -0400

    Update picam-test.py

--- picam-test.py
+++ picam-test.py
@@ -5,12 +5,15 @@ camera = PiCamera()
 camera.hflip=True
 camera.vflip=True
 
-for i in range(3):
-	sleep(3)
-	camera.capture('/home/pi/picam-test/image%s.jpg' % i)
+for i in range(5):
+	sleep(1)
+	#camera.capture('/home/pi/picam-test/image%s.jpg' % i)
+	camera.capture('image{0:04d}.jpg'.format(i))
 	
-camera.start_recording('video.h264')
-sleep (5)
-camera.stop_recording()
+
+	
+#camera.start_recording('video.h264')
+#sleep (5)
+#camera.stop_recording()
 
 print("done")
