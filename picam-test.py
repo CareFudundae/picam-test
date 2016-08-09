2b40702be19108dcf754a408d535a7a55f442092
levisonofed <levi.j.elston@gmail.com>
2016-08-08 20:26:07 -0400

    Update picam-test.py

--- picam-test.py
+++ picam-test.py
@@ -1,10 +1,14 @@
 from picamera import PiCamera
 from time import sleep
-
 camera = PiCamera()
+
 camera.hflip=True
 camera.vflip=True
 
 for i in range(3):
 	sleep(3)
 	camera.capture('/home/pi/picam-test/image%s.jpg' % i)
+	
+camera.start_recording('video.h264')
+sleep (5)
+camera.stop_recording()
